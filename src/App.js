1955012a8e9729a360ac994b8867fc32c4254ce9
Pablo Mich <pabloemich@gmail.com>
2022-04-19 20:18:22 -0600

    subiendo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div className="App">
+        <p class="titulo">Background Color:</p>
+        <div class="boton">
+          <button class="btn">Click Me</button>
+        </div>
+      </div>
+    </body>
   );
+}
+
+const btn = document.querySelector('button');
+const body = document.querySelector('body');
+const p = document.querySelector('p')
+
+const colores = [
+  'blue',
+  'yellow',
+  'green',
+  'red',
+  'orange',
+  'purple',
+  'pink',
+  'brown',
+  'gray',
+  'Highlight',
+  'antiquewhite',
+  'aqua',
+  'blueviolet',
+  'cadetblue',
+  'chartreuse',
+  'coral',
+  'crimson',
+  'darkkhaki',
+  'darkorange',
+  'deeppink',
+  'khaki',
+  'lawngreen',
+  'lightblue',
+  'lightcoral',
+  'steelblue',
+  'thistle',
+  'wheat',
+  'whitesmoke',
+  'violet',
+  'teal',];
+
+btn.addEventListener('click', fondo);
+
+function fondo() {
+  var indice = parseInt(Math.random() * colores.length);
+  var nameColor = colores[indice];
+  body.style.backgroundColor = colores[indice];
+  p.innerText = "Background Color: " + nameColor;
 }
 
 export default App;
